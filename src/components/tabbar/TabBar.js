0ff49d1f98ad3487305d980e935aa6f38b70b7ae
MarinGunner <1318470860@qq.com>
2019-08-22 16:52:53 +0800

    updata iconfont

--- src/components/tabbar/TabBar.js
+++ src/components/tabbar/TabBar.js
@@ -5,9 +5,9 @@ import './style.scss'
 
 export default (props) => {
     const navList = [
-        { id: 1, name: '职业', path: '/job', },
-        { id: 2, name: '搜索', path: '/search' },
-        { id: 3, name: '我的', path: '/mine' }
+        { id: 1, name: '职业', path: '/job',icon:'iconshouye'},
+        { id: 2, name: '搜索', path: '/search',icon:'iconsearch'},
+        { id: 3, name: '我的', path: '/mine',icon:'iconwode'}
     ];
     return (
         <nav className='tab-bar border-top'>
@@ -15,6 +15,7 @@ export default (props) => {
             {
                 navList.map(item => (
                     <NavLink className='tab-item' key={item.id} to={item.path}>
+                        <span className={`iconfont ${item.icon}`}></span>
                         {item.name}
                     </NavLink>
                 ))
