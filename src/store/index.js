1e512d16da5ed998974c1214d64a814c8f2f48bb
MarinGunner <1318470860@qq.com>
2019-08-24 11:17:27 +0800

    updata login

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,18 @@
-import { createStore, combineReducers, compose } from 'redux'
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+// 引入各个reducer模块
+import user from './modules/user'
 
-//引入reducer模块
-import user from './modules/user'   
-
-//合并reducer
+// 合并多个reducer
 const reducer = combineReducers({
     user,
 });
 
-//使用开发者工具：
+// 使用redux开发者工具
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default createStore(reducer, composeEnhancers())
+// 输出store
+export default createStore(reducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
+
